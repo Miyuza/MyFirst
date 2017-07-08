1637c51b8e2e26d9242835914393f48b269c0aa0
Miyuza <sipteu@gmail.com>
2017-07-08 14:45:03 +0500

    1st

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String u = "maham";
+            int a = u.GetHashCode();
+            Console.WriteLine(a);
+            String p = "12345";
+            int b = p.GetHashCode();
+            Console.WriteLine(b);
+            String p1 = "12345";
+            int b1 = p1.GetHashCode();
+            Console.WriteLine(b1);
+            Console.WriteLine(DateTime.Now);
+            Guid g;
+            g = Guid.NewGuid();
+            String str = g.ToString();
+            Console.WriteLine(g);
+            Console.WriteLine(Guid.NewGuid());
+            Console.WriteLine(str);
+            String[] ab = str.Split('-');
+            Console.WriteLine(ab[0]);
+        }
+    }
+}
